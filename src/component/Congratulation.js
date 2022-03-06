1d70df28687d380fd7c69782d67c8da5b9e092dc
amirSh10 <93071645+amirSh10@users.noreply.github.com>
2022-03-06 14:38:05 +0330

    set web to multi language

--- src/component/Congratulation.js
+++ src/component/Congratulation.js
@@ -4,28 +4,73 @@ function Cangratulation() {
   const [click, setClick] = useState("show");
   const [Value, setValue] = useState("");
   const [err, setErr] = useState("noErr");
+  const [language, setLanguage] = useState("english");
+
+  let content = {
+    english: {
+      text1: " You did it! So proud of you.",
+      text2: "Congratulation",
+      text_btn: " Congratulation",
+      text_err: " Fill the input!",
+      text_placeholder: "Enter Your Name",
+    },
+    persian: {
+      text1: ".توانجامش دادی! خیلی به تو افتخار میکنم",
+      text2: "تبریک",
+      text_btn: "تبریک",
+      text_err: "نام را پر کنید!",
+      text_placeholder: "نام خود را وارد کنید",
+    },
+  };
+
+  language == "english"
+    ? (content = content.english)
+    : (content = content.persian);
 
   return (
     <>
       <div className="flex_of_congratulation">
         <div className="flex_main_div_of_congratulation">
-          {/* <h1 className="style_of_text_congratulation">Congratulation</h1> */}
+          <div className="flex_of_language">
+            <div class="dropdown">
+              <button class="dropbtn">Language &#10061;</button>
+              <div class="dropdown-content">
+                <a
+                  href="#"
+                  onClick={() => {
+                    setLanguage("english");
+                  }}
+                >
+                  English
+                </a>
+                <a
+                  href="#"
+                  onClick={() => {
+                    setLanguage("persian");
+                  }}
+                >
+                  Persian
+                </a>
+              </div>
+            </div>
+          </div>
           <h1 className="style_of_text_congratulation">
-            You did it! So proud of you.
+            {/* You did it! So proud of you. */}
+            {content.text1}
           </h1>
 
           <h1
             className="style_of_text_congratulation aa"
             style={{ display: click == "show" ? "none" : "block" }}
           >
-            Congratulation <span style={{ color: "#FBA100" }}>{Value}</span>
+            {content.text2} <span style={{ color: "#FBA100" }}>{Value}</span>
           </h1>
           <div style={{ display: click == "show" ? "block" : "none" }}>
             <div className="flex_of_imput_congratulation">
               <input
                 className="style_input_congratulation"
                 type="text"
-                placeholder="Enter Your Name"
+                placeholder={content.text_placeholder}
                 required="required"
                 onChange={(x) => setValue(x.target.value)}
               ></input>
@@ -45,7 +90,7 @@ function Cangratulation() {
                   }
                 }}
               >
-                Congratulation
+                {content.text_btn}
               </button>
             </div>
             <h6
@@ -55,7 +100,7 @@ function Cangratulation() {
                 marginTop: "50px",
               }}
             >
-              Fill the input
+              {content.text_err}
             </h6>
           </div>
         </div>
@@ -64,7 +109,4 @@ function Cangratulation() {
   );
 }
 
-// function Err(props){
-//   return<h1 style={{display:err=="err"?"block":"none"}}>Fill the input</h1>
-// }
 export default Cangratulation;
